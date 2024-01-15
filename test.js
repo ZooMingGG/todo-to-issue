8ad6a833a0d99f02e464121d40531508e413d17b
Volodymyr Lukashevych <lukashevych.volodymyr@gmail.com>
2024-01-15 13:23:16 +0200

    test

---
+++ test.js
@@ -0,0 +1,41 @@
+function sectionA() {
+  for (let i = 0; i < 20; i++) {
+    // Some code in section A
+  }
+}
+
+function sectionB() {
+  for (let j = 0; j < 20; j++) {
+    // Some code in section B
+  }
+}
+
+// TODO: add unit tests
+function sectionC() {
+  for (let k = 0; k < 20; k++) {
+    // Some code in section C
+  }
+}
+
+function sectionD() {
+  for (let l = 0; l < 20; l++) {
+    // Some code in section D
+  }
+}
+
+function sectionE() {
+  for (let m = 0; m < 20; m++) {
+    // Some code in section E
+  }
+}
+
+sectionA();
+sectionB();
+sectionC();
+sectionD();
+sectionE();
+sectionA();
+sectionB();
+sectionC();
+sectionD();
+sectionE();
